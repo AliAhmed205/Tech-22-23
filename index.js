224cf23b47eaf386b3b890d8281a499831665a05
AliAhmed205 <ali_lazuli@outlook.com>
2023-03-14 17:51:52 +0100

    Updating mijn repo

--- index.js
+++ index.js
@@ -1,43 +1,153 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const ejs = require('ejs')
-const app = express()
-const multer = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+require('dotenv').config({path: '.env'});
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const app = express();
+const multer = require('multer');
+const upload = multer({ dest: 'public/uploads/' });
+const { MongoClient, ObjectId } = require('mongodb');
 
 // Set up middleware
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.set('view engine', 'ejs');
 app.use('/public/', express.static('./public'));
 app.use(express.static(__dirname + '/public'));
 
 // In-memory store for themes
-let themes = []
+let themes = [];
 
-// Render the theme builder form
-app.get('/', (req, res) => {
-  res.render('theme-builder', { themes: themes })
-})
+let collection;
 
-// Handle the theme builder request
-app.post('/api/theme-builder', upload.single('image'), (req, res) => {
-  const { body, file } = req
-  const theme = {
-    name: body.name,
-    backgroundColor: body.backgroundColor,
-    fontFamily: body.fontFamily,
-    image: file ? file.filename : null,
+const uri = "mongodb+srv://AliLazuli123:Lazuur123@cluster0.zugcsqg.mongodb.net/mydatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Connect to the database and set up the collection
+async function connectToDatabase() {
+  try {
+    await client.connect();
+    collection = client.db("mydatabase").collection("themes");
+    console.log("Connected to database");
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
   }
-  // Save the theme to your database
-  themes.push(theme)
-  console.log('Theme builder request received:', body)
-  console.log('Uploaded image:', file)
-  res.redirect('/')
-})
+}
+
+connectToDatabase();
+
+// Insert a new theme into the database
+async function insertTheme(theme) {
+  try {
+    const result = await collection.insertOne(theme);
+    console.log('Theme saved to database:', theme);
+    themes.push(theme);
+    return result;
+  } catch (err) {
+    console.log(err);
+    throw err;
+  }
+}
+
+app.post('/submit-form', upload.single('image'), async (req, res) => {
+  console.log('Your coven theme has been uploaded, huzzah!')
+  console.log(req.file)
+  const { body, file } = req;
+  const theme = {
+    _id: body.id,
+    name: body.name, 
+    backgroundColor: body.color,
+    fontFamily: body.font,
+    textColor: body['font-color'],
+    images:  file.filename,
+    thumbnailUrl: `/public/uploads/${file.filename}`
+  };
+  
+  try {
+    await insertTheme(theme);
+    try {
+      const renderData = await collection.find({}).toArray();
+      res.render('theme-builder2', { themes: renderData , theme});
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Failed to retrieve themes');
+    }
+  } catch (err) {
+    res.status(500).send({ error: 'Failed to save theme' });
+  }
+});
+
+app.get('/', async (req, res) => {
+  if (!collection) {
+    return res.status(500).send('Unable to connect to database');
+  }
+  
+  try {
+    const renderData = await collection.find({}).toArray();
+    res.render('theme-builder', { themes: renderData });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Failed to retrieve themes');
+  }
+});
+
+app.get('/themes/:themeID', async (req, res) => {
+  try {
+    if (!collection) {
+      return res.status(500).send('Unable to connect to database');
+    }
+
+    const id = req.params.themeID;
+    const theme = await collection.findOne({ _id: new ObjectId(id) });
+    if (!theme) {
+      return res.status(404).send('Theme not found');
+    }
+
+    const renderData = await collection.find({}).toArray(); // hier is een aanpassing gemaakt
+    res.render('theme-builder2', { theme, themes: renderData }); // hier is een aanpassing gemaakt
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Failed to retrieve theme');
+  }
+});
+
+app.delete('/themes/:themeID', async (req, res) => {
+  try {
+    if (!collection) {
+      return res.status(500).send('Unable to connect to database');
+    }
+
+    const id = req.params.themeID;
+    const result = await collection.deleteOne({ _id: new ObjectId(id) });
+    if (result.deletedCount === 0) {
+      return res.status(404).send('Theme not found');
+    }
+
+    res.redirect('/');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Failed to delete theme');
+  }
+});
+
+
+
+
+app.get('/form', (req, res) => {
+  res.render('form.ejs');
+});
+
+
+app.post('/submit', (req, res) => {
+  const name = req.body.test;
+  res.send(`Name: ${name}`);
+});
+
 
 // Start the server
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`)
-})
+  console.log(`Server listening on port ${PORT}`);
+  console.error();
+});
--- public/javascript/app.js
+++ public/javascript/app.js
@@ -1,29 +1,32 @@
-const form = document.getElementById("theme-builder-form")
+    const form = document.querySelector('form');
+    const nameInput = document.querySelector('#name');
+    const colorInput = document.querySelector('#color');
+    const fileInput = document.querySelector('#image-upload-input');
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault()
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
 
-  const formData = new FormData(form)
+        // Create a new button element for the theme
+        const button = document.createElement('button');
+        const thumbnail = document.createElement('img');
+        thumbnail.src = URL.createObjectURL(fileInput.files[0]);
+        thumbnail.alt = nameInput.value + ' Theme Icon';
+        button.appendChild(thumbnail);
+        button.appendChild(document.createTextNode(nameInput.value));
+        button.addEventListener('click', () => {
+            // Replace the header image with the uploaded image
+            document.querySelector('header img').src = thumbnail.src;
+            // Replace all theme buttons' thumbnails with the uploaded image
+            document.querySelectorAll('section:nth-of-type(2) button img').forEach((img) => {
+                img.src = thumbnail.src;
+            });
+            // Change the background color of the first and third sections
+            document.querySelector('section:first-of-type').style.backgroundColor = colorInput.value;
+            document.querySelector('section:nth-of-type(3)').style.backgroundColor = colorInput.value;
+            // Change the font color of the third section
+            document.querySelector('section:nth-of-type(3) p').style.color = document.querySelector('#font-color').value;
+        });
 
-  fetch("/api/theme-builder", {
-    method: "POST",
-    body: formData,
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok")
-      }
-      return response.json()
-    })
-    .then((data) => {
-      console.log(data)
-      // Update the UI with the new theme
-      document.body.style.backgroundColor = formData.get("bg-color")
-      document.body.style.fontFamily = formData.get("font-family")
-      const imageUrl = URL.createObjectURL(formData.get("image"))
-      document.body.style.backgroundImage = `url(${imageUrl})`
-    })
-    .catch((error) => {
-      console.error("There was an error submitting the form:", error)
-    })
-})
+        // Add the new button to the theme list
+        document.querySelector('section:nth-of-type(2)').appendChild(button);
+    });
